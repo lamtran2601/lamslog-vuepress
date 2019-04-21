124c6a62fe0bdb233051f2c73447696e186b1b03
Lam Tran <lamtran2601@gmail.com>
2019-04-21 15:09:15 +0700

    update build

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'LamsLog',
   description: "Lam's note something",
   head: [
-    ['link', { rel: 'shortcut icon', href: './logo.png' }]
+    ['link', { rel: 'shortcut icon', href: 'logo.png' }]
   ],
-  dest: './docs'
+  dest: 'docs'
 }
