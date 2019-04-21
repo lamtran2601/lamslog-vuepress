f9cc98e8a8315e5a3ebb5c3856029e85e666d5b6
Lam Tran <lamtran2601@gmail.com>
2019-04-21 14:23:08 +0700

    add config + first build

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'LamsLog',
+  description: "Lam's note something",
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/logo.png' }]
+  ],
+  dest: '.vuepress/dist'
+}
